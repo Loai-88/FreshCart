7255de161c850045a9951d6d45d6682a25ef00be
loai hesham <loaihesham581@gmail.com>
2025-03-03 22:13:36 +0200

    handel logOut in drop

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -156,7 +156,7 @@ export default function Navbar() {
                                 <div className="py-6">
 
                                     {
-                                        UserToken ? <NavLink className={style.animated2}>
+                                        UserToken ? <a onClick={() => Logout()}  className={style.animated2}>
                                             <svg viewBox="0 0 24 24" className={style.arr2} xmlns="http://www.w3.org/2000/svg">
                                                 <path d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z" />
                                             </svg>
@@ -165,7 +165,7 @@ export default function Navbar() {
                                             <svg viewBox="0 0 24 24" className={style.arr1} xmlns="http://www.w3.org/2000/svg">
                                                 <path d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z" />
                                             </svg>
-                                        </NavLink>
+                                        </a>
                                             :
                                             <>
                                                 <NavLink to={'login'} className={style.animated}>
